0830d25d62f598b571260515afd8d966cc091fa2
Vidhi <vidhidoesnotreply@gmail.com>
2024-06-19 17:22:27 +0530

    done for submission

--- urban_farming_model.py
+++ urban_farming_model.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
-from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold, cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn import metrics
 import optuna
 from xgboost import XGBClassifier
-from sklearn.svm import SVC
 import mlflow
 import mlflow.sklearn
 import os
@@ -62,63 +58,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 print("Y_Train value counts:", y_train.value_counts())
 
-# RandomForest
-rf = RandomForestClassifier(random_state=RANDOM_SEED)
-param_grid_forest = {
-    'n_estimators': [200, 400, 700],
-    'max_depth': [10, 20, 30],
-    'criterion': ["gini", "entropy"],
-    'max_leaf_nodes': [50, 100]
-}
-
-cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=RANDOM_SEED)
-
-grid_forest = GridSearchCV(
-    estimator=rf,
-    param_grid=param_grid_forest,
-    cv=cv,
-    n_jobs=-1,
-    scoring='accuracy',
-    verbose=0
-)
-model_forest = grid_forest.fit(X_train, y_train)
-
-# Logistic Regression
-lr = LogisticRegression(random_state=RANDOM_SEED)
-param_grid_log = {
-    'C': [100, 10, 1.0, 0.1, 0.01],
-    'penalty': ['l1', 'l2'],
-    'solver': ['liblinear']
-}
-
-grid_log = GridSearchCV(
-    estimator=lr,
-    param_grid=param_grid_log,
-    cv=5,
-    n_jobs=-1,
-    scoring='accuracy',
-    verbose=0
-)
-model_log = grid_log.fit(X_train, y_train)
-
-# Decision Tree
-dt = DecisionTreeClassifier(random_state=RANDOM_SEED)
-
-param_grid_tree = {
-    "max_depth": [3, 5, 7, 9, 11, 13],
-    'criterion': ["gini", "entropy"],
-}
-
-grid_tree = GridSearchCV(
-    estimator=dt,
-    param_grid=param_grid_tree,
-    cv=5,
-    n_jobs=-1,
-    scoring='accuracy',
-    verbose=0
-)
-model_tree = grid_tree.fit(X_train, y_train)
-
 # Hyperparameter Optimization with Optuna for XGBClassifier
 def objective(trial):
     param = {
@@ -150,10 +89,6 @@ print('Best trial:', study.best_trial.params)
 best_params = study.best_trial.params
 xgb_model = XGBClassifier(**best_params)
 xgb_model.fit(X_train, y_train)
-
-# Train SVM model
-svm_model = SVC(probability=True)
-svm_model.fit(X_train, y_train)
 
 # Model evaluation metrics
 def eval_metrics(actual, pred, pred_proba=None):
@@ -197,7 +132,6 @@ def mlflow_logging(model, X, y, name, use_proba=False):
         if hasattr(model, 'best_params_'):
             mlflow.log_params(model.best_params_)
         # Log the metrics
-        mlflow.log_metric("Mean CV score", model.best_score_ if hasattr(model, 'best_score_') else float('nan'))
         mlflow.log_metric("Accuracy", accuracy)
         mlflow.log_metric("f1-score", f1)
         mlflow.log_metric("Precision", precision)
@@ -211,8 +145,4 @@ def mlflow_logging(model, X, y, name, use_proba=False):
         
         mlflow.end_run()
 
-# mlflow_logging(model_tree, X_test, y_test, "DecisionTreeClassifier")
-# mlflow_logging(model_log, X_test, y_test, "LogisticRegression")
-# mlflow_logging(model_forest, X_test, y_test, "RandomForestClassifier")
 mlflow_logging(xgb_model, X_test, y_test, "XGBClassifier", use_proba=True)
-# mlflow_logging(svm_model, X_test, y_test, "SVM", use_proba=True)
